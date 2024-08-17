28eac0be3d159cb2a6cee51547b2c380d44fa664
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-08-16 23:17:33 -0500

    taskRepo

--- src/main/java/riwi/example/taskManagement/repositories/TaskRepository.java
+++ src/main/java/riwi/example/taskManagement/repositories/TaskRepository.java
@@ -1,4 +1,9 @@
 package riwi.example.taskManagement.repositories;
 
-public class TaskRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import riwi.example.taskManagement.entities.TaskEntity;
+
+@Repository
+public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
 }
